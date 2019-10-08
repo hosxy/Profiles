b62f2c5c054e5b653492afcefc7834366a408780
hosxy <hosxy@live.com>
2019-10-08 16:31:40 +0800

    Update config.fish

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -6,3 +6,26 @@ set -x PATH $PATH ~/.local/bin
 
 #设置默认文本编辑器为 vim
 set -x EDITOR vim
+
+# 输入法相关
+set -x QT_IM_MODULE yong
+set -x GTK_IM_MODULE yong
+set -x XMODIFIERS @im=yong
+
+# intel硬解驱动
+set -x LIBVA_DRIVER_NAME iHD
+
+# 设定 XDG 相关目录
+set -x XDG_CONFIG_HOME ~/.config
+set -x XDG_DATA_HOME ~/.local/share
+set -x XDG_CACHE_HOME ~/.cache
+
+# 管理 dotfile/dotdir
+set -x GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
+set -x ANDROID_SDK_HOME $XDG_CONFIG_HOME/android
+
+set -x GNUPGHOME $XDG_DATA_HOME/gnupg
+set -x HISTFILE $XDG_DATA_HOME/bash/history
+set -x BASH_COMPLETION_USER_FILE $XDG_DATA_HOME/bash/bash_completion
+
+#set -x XAUTHORITY $XDG_CACHE_HOME/Xauthority
