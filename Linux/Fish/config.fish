8f366fe51e99b91b203f812d08d1bc19e65b3ac0
hosxy <hosxy@live.com>
2019-11-27 13:47:42 +0800

    move to .pam_environment  to make universal

--- Linux/Fish/config.fish
+++ Linux/Fish/config.fish
@@ -1,24 +1,5 @@
 # 关闭欢迎语
 set -U fish_greeting 
 
-# 添加环境变量
-set -x PATH $PATH ~/.local/bin
-
-# 设定 XDG 相关目录
-set -x XDG_CONFIG_HOME ~/.config
-set -x XDG_DATA_HOME ~/.local/share
-set -x XDG_CACHE_HOME ~/.cache
-
-# 输入法相关
-set -x GTK_IM_MODULE fcitx
-set -x QT_IM_MODULE fcitx
-set -x XMODIFIERS @im=fcitx
-
-# intel硬解驱动
-set -x LIBVA_DRIVER_NAME iHD
-
 # 使用 nvidia 驱动运行
 alias nvrun "env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia"
-
-# 管理 dotfile/dotdir
-source $XDG_CONFIG_HOME/fish/dotfile.fish
